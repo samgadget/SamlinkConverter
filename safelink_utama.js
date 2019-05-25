e7ada857ef4ef08eea5997f63281e865710fb299
SamGadget <51016232+samgadget@users.noreply.github.com>
2019-05-25 14:22:42 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="direct-safelink.html"}
 if(x=="2"){xxx="direct-safelink.html"}
 if(x=="3"){xxx="direct-safelink.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://samlinkconverter.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="direct-safelink.html"}a_to_ve[i].href="https://samlinkconverter.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
